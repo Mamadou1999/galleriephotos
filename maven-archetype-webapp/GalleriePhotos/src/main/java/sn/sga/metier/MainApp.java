528470c9328f04737e9c588e81dd5b5d1bc72655
Mamadou1999 <diopmamadou706@gmail.com>
2021-02-18 17:30:52 +0000

    setting up the environment

---
+++ maven-archetype-webapp/GalleriePhotos/src/main/java/sn/sga/metier/MainApp.java
@@ -0,0 +1,26 @@
+package sn.sga.metier;
+
+import sn.sga.utils.*;
+import javax.persistence.EntityManager;
+
+public class MainApp {
+
+	public static void main(String[] args) 
+	{
+		EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	    entityManager.getTransaction().begin();
+
+	    // Check database version
+	    String sql = "select version()";
+
+	    String result = (String) entityManager.createNativeQuery(sql).getSingleResult();
+	    System.out.println(result);
+
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	    JPAUtil.shutdown();
+
+	}
+
+}
